9f23bbf5fcaca07f61e0abd56df218711641894f
seugirdorx <amandagetion@gmail.com>
2022-11-25 08:53:58 -0300

    navbar_home_footer

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,62 @@
+import React from "react";
+import  './Home.css'
+import {Grid, Paper, Button, Typography} from '@material-ui/core'
+import {Box} from '@mui/material'
+
+function Home(){
+    return(
+        <>
+
+            <Grid container direction="row" justifyContent="center" alignItems="center" style={{ backgroundColor: "#3F51B5" }}>
+                <Grid alignItems="center" item xs={6}>
+                    <Box paddingX={20} >
+                        <Typography variant="h3" gutterBottom color="textPrimary" component="h3" align="center" style={{ color: "white", fontWeight: "bold" }}>Seja bem vindo(a)!</Typography>
+                        <Typography variant="h5" gutterBottom color="textPrimary" component="h5" align="center" style={{ color: "white", fontWeight: "bold" }}>expresse aqui os seus pensamentos e opiniões!</Typography>
+                    </Box>
+                    <Box display="flex" justifyContent="center">
+                        <Box marginRight={1}>
+                        </Box>
+                        <Button variant="outlined" style={{ borderColor: "white", backgroundColor: "#3F51B5", color: "white" }}>Ver Postagens</Button>
+                    </Box>
+                </Grid>
+                <Grid item xs={6} >
+                    <img src="https://i.imgur.com/H88yIo2.png" alt="" width="500px" height="500px" />
+                </Grid>
+                <Grid xs={12} style={{ backgroundColor: "white" }}>
+                </Grid>
+            </Grid>
+
+
+            {/* <Paper>
+            <Box p={2}>
+                <Box display='flex' justifyContent='center'>
+                    <h1>O HEXA VEM !!!!!!</h1>
+                </Box>
+                <img src="https://www.visa.com.br/dam/VCOM/regional/lac/brazil/newsroom/visa-traz-trofeu-do-campeao-da-copa-do-mundo-da-FIFA-800x450.jpg" alt="trofeu" style={{width: "100%", height: "100%"}}/>
+                <Box display="flex" justifyContent="center" p={2}>
+                    <Button variant='contained' color="primary"> obvio!</Button>
+                    <Button variant='contained' color="secondary">nao</Button>
+                </Box>
+            </Box>
+            </Paper> */}
+            
+
+        {/* <Grid container spacing={2}>
+            <Grid item xs={12} sm={8}>
+                <Paper style={{height: "100vh", background: "blue"}}/>
+            </Grid>
+            <Grid item container direction='column' xs={12} sm={4} spacing={2}>
+                <Grid item>
+                    <Paper style={{height: "49vh", background: "yellow"}}/>
+                </Grid>
+                <Grid item>
+                    <Paper style={{height: "49vh", background: "green"}}/>
+                </Grid>
+            </Grid>
+        </Grid> */}
+
+        </>
+    );
+}
+
+export default Home;
